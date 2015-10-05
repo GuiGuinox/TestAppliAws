99cf86cb6c1bb51375b21d29921d0d3707a5c23f
Guillaume Jourdain <guiguinox@gmail.com>
2015-10-05 11:33:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var port = 8080;
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write('1ere instance Guillaume !');
+res.end();
+}).listen(port);
+console.log('Serveur tourne sur http://localhost:' +port );
