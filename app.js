cee53a047f192ea018ff34074486c29125b78454
Guillaume Jourdain <guiguinox@gmail.com>
2015-10-05 14:19:13 +0200

    Change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 var port = 80;
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
-res.write('1ere instance Guillaume !');
+res.write('2eme instance Guillaume !');
 res.end();
 }).listen(port);
 console.log('Serveur tourne sur http://localhost:' +port );
